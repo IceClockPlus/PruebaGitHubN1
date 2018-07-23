52ddad77ea59e4afe23e86ece6cbab982847cf91
IceClockPlus <39346738+IceClockPlus@users.noreply.github.com>
2018-07-23 18:38:51 -0400

    Primera Version de Prueba

---
+++ ConsolaGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es la prueba de la version 1");
+            Console.ReadKey();
+        }
+    }
+}
