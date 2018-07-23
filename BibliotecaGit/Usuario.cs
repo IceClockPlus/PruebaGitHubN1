a19d4bc2aebf331a9c299b821e7dadb4d25cfc63
IceClockPlus <39346738+IceClockPlus@users.noreply.github.com>
2018-07-23 19:01:26 -0400

    Creacion de una clase Usuario en la Biblioteca BibliotecaGit

---
+++ BibliotecaGit/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGit
+{
+    class Usuario
+    {
+        private string Nombre;
+
+        public Usuario()
+        {
+            this.Init();
+        }
+
+        private void Init()
+        {
+            this.Nombre = String.Empty;
+        }
+    }
+}
